a5d56ca6714443a08e69d3d5a191fdc350764f7d
austinare <austinjrichards@outlook.com>
2021-01-02 16:30:34 -0700

    made my first window with tkinter!

---
+++ calculator.py
@@ -0,0 +1,18 @@
+'''
+Austin Richards 1/2/21 16:21
+'''
+# making a window with tkinter
+from tkinter import *
+
+class Window(Frame):
+
+    def __init__(self, master = None):
+        Frame.__init__(self, master)
+
+        self.master = master
+
+root = Tk()
+
+app = Window(root)
+
+root.mainloop()
