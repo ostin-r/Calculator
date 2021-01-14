032d8a53e6ba39a5527b112ed4ca8ff7418ad96b
austinare <austinjrichards@outlook.com>
2021-01-13 19:29:19 -0700

    finished themed calculator!

--- themed_calculator.py
+++ themed_calculator.py
@@ -10,36 +10,104 @@ from tkinter import ttk as tk
 from ttkthemes import ThemedTk
 
 # initialize the themed window
-gui = ThemedTk(theme='elegance')
+gui = ThemedTk(theme='clearlooks')
 gui.title('Simple Calculator')
 
 # display
 display = tk.Entry(width=30)
-display.grid(row=0, column=0, columnspan=4, sticky="nesw")
+display.grid(row=0, column=0, columnspan=4, sticky="nsew")
+
+# functions for the buttons
+def button_click(number):
+    current = display.get()
+    display.delete(0, END)
+    display.insert(0, str(current) + str(number))
+
+def clear_display():
+    display.delete(0, END)
+
+def sign_change():
+    numb = display.get()
+    display.delete(0, END)
+    display.insert(0, "-" + str(numb))
+
+def button_add():
+    first_number = display.get()
+    global global_num
+    global math
+    math = "add"
+    global_num = float(first_number)
+    display.delete(0, END)
+
+def button_sub():
+    first_number = display.get()
+    global global_num
+    global math
+    math = "sub"
+    global_num = float(first_number)
+    display.delete(0, END)
+
+def button_mul():
+    first_number = display.get()
+    global global_num
+    global math
+    math = "mul"
+    global_num = float(first_number)
+    display.delete(0, END)
+
+def button_div():
+    first_number = display.get()
+    global global_num
+    global math
+    math = "div"
+    global_num = float(first_number)
+    display.delete(0, END)
+
+def button_equal():
+    second_number = float(display.get())
+    display.delete(0, END)
+
+    if math == "add":
+        display.insert(0, global_num + second_number)
+    
+    elif math == "sub":
+        display.insert(0, global_num - second_number)
+
+    elif math == "mul":
+        display.insert(0, global_num * second_number)
+
+    elif math == "div":
+        if second_number == 0:
+            display.insert(0, "divide by 0 error")
+        else:
+            display.insert(0, global_num/second_number)
+
+    else:
+        display.insert(0, 'error')
 
 # make the buttons
-gen_width=10
+gen_width=6
 
-but1 = tk.Button(gui, text='1', width=gen_width)
-but2 = tk.Button(gui, text='2', width=gen_width)
-but3 = tk.Button(gui, text='3', width=gen_width)
-but4 = tk.Button(gui, text='4', width=gen_width)
-but5 = tk.Button(gui, text='5', width=gen_width)
-but6 = tk.Button(gui, text='6', width=gen_width)
-but7 = tk.Button(gui, text='7', width=gen_width)
-but8 = tk.Button(gui, text='8', width=gen_width)
-but9 = tk.Button(gui, text='9', width=gen_width)
-but0 = tk.Button(gui, text='0', width=gen_width)
+but1 = tk.Button(gui, text='1', width=gen_width, command=lambda: button_click(1))
+but2 = tk.Button(gui, text='2', width=gen_width, command=lambda: button_click(2))
+but3 = tk.Button(gui, text='3', width=gen_width, command=lambda: button_click(3))
+but4 = tk.Button(gui, text='4', width=gen_width, command=lambda: button_click(4))
+but5 = tk.Button(gui, text='5', width=gen_width, command=lambda: button_click(5))
+but6 = tk.Button(gui, text='6', width=gen_width, command=lambda: button_click(6))
+but7 = tk.Button(gui, text='7', width=gen_width, command=lambda: button_click(7))
+but8 = tk.Button(gui, text='8', width=gen_width, command=lambda: button_click(8))
+but9 = tk.Button(gui, text='9', width=gen_width, command=lambda: button_click(9))
+but0 = tk.Button(gui, text='0', width=gen_width, command=lambda: button_click(0))
 
-but_add = tk.Button(gui, text='+', width=gen_width)
-but_sub = tk.Button(gui, text='-', width=gen_width)
-but_mul = tk.Button(gui, text='x', width=gen_width)
-but_div = tk.Button(gui, text='/', width=gen_width)
+but_add = tk.Button(gui, text='+', width=gen_width, command=button_add)
+but_sub = tk.Button(gui, text='-', width=gen_width, command=button_sub)
+but_mul = tk.Button(gui, text='x', width=gen_width, command=button_mul)
+but_div = tk.Button(gui, text='/', width=gen_width, command=button_div)
 
-but_decimal = tk.Button(gui, text='.', width=gen_width)
-but_sign = tk.Button(gui, text='+/-', width=gen_width)
-but_clear = tk.Button(gui, text='clear')
-but_equal = tk.Button(gui, text='=')
+but_decimal = tk.Button(gui, text='.', width=gen_width, command=lambda: button_click("."))
+but_sign    = tk.Button(gui, text='+/-', width=gen_width, command=sign_change)
+but_clear   = tk.Button(gui, text='clear', command=clear_display)
+but_equal   = tk.Button(gui, text='=', command=button_equal)
 
 # put the buttons in the window
 but9.grid(row=1, column=2)
